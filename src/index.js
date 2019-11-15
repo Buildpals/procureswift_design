955d885f7ca86dea5490afaf847ad7fc2cf52b4e
Peter O'Hara Adu <ohara.invent@gmail.com>
2019-11-15 20:21:43 +0000

    fix: Some stuff

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
-import './scss/app.scss';
-import 'bootstrap';
+import './scss/app.scss'
+import 'bootstrap'
 
 import '@fortawesome/fontawesome-free/js/fontawesome'
 import '@fortawesome/fontawesome-free/js/solid'
-import '@fortawesome/fontawesome-free/js/regular'
+// import '@fortawesome/fontawesome-free/js/regular'
 import '@fortawesome/fontawesome-free/js/brands'
+
+
+
+import * as intlTelInput from 'intl-tel-input'
+
+const input = document.querySelector("#phone");
+const instance = intlTelInput(input, {initialCountry: "gh"});
+
