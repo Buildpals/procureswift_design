63a54c05979a3339a1a9a6258b66d66ba74f0691
Peter O'Hara Adu <ohara.invent@gmail.com>
2019-11-15 18:40:03 +0000

    feat: Add webpack for bootstrap customization

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import './scss/app.scss';
+import 'bootstrap';
+
+import '@fortawesome/fontawesome-free/js/fontawesome'
+import '@fortawesome/fontawesome-free/js/solid'
+import '@fortawesome/fontawesome-free/js/regular'
+import '@fortawesome/fontawesome-free/js/brands'
