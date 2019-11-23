a3549c781000e01a109ad92422aff95ffef32186
Peter O'Hara Adu <ohara.invent@gmail.com>
2019-11-23 12:21:26 +0000

    fix: Reduce size of main.js by using Tree-shaking for fontawesome

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import './scss/app.scss'
 import 'bootstrap'
 
-import '@fortawesome/fontawesome-free/js/fontawesome'
-import '@fortawesome/fontawesome-free/js/solid'
-// import '@fortawesome/fontawesome-free/js/regular'
-import '@fortawesome/fontawesome-free/js/brands'
-
-
-
+import {dom, library} from '@fortawesome/fontawesome-svg-core'
+import {faHandHoldingUsd, faTruck} from '@fortawesome/free-solid-svg-icons'
+import {faAmazon, faFacebook, faInstagram} from '@fortawesome/free-brands-svg-icons'
 import * as intlTelInput from 'intl-tel-input'
 
+// We are only using the faTruck, faHandHoldingUsd, faAmazon, faFacebook, faInstagram icons
+library.add(faTruck, faHandHoldingUsd, faAmazon, faFacebook, faInstagram);
+
+// Replace any existing <i> tags with <svg> and set up a MutationObserver to
+// continue doing this as the DOM changes.
+dom.watch();
+
+// Change initialize intlInput on all elements with the id phone
 const input = document.querySelector("#phone");
 const instance = intlTelInput(input, {initialCountry: "gh"});
 
